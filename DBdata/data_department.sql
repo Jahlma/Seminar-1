1bff5535c2d1cb1ff548cf660c12178db9c25a48
Jahlma <jahlma@kth.se>
2025-11-20 18:50:35 +0100

    Add files via upload

---
+++ DBdata/data_department.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (department_name,manager)
+VALUES
+  ('STOCKHOLM','MR GREEN'),
+  ('GÖTEBORG','MR SVENSSON'),
+  ('MALMÖ','KAJSA'),
+  ('UPPSALA','HERMANN'),
+  ('SÖDERTÄLJE','LOVA');
---
+++ DBdata/data_teaching_activity.sql
@@ -0,0 +1,6 @@
+INSERT INTO teaching_activity (activity_name,factor)
+VALUES
+  ('LAB',2.3),
+  ('SEMINAR',2.5),
+  ('LECTURE',1.5),
+  ('Tutorial',2);
