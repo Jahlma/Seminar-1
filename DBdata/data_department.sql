8d16ac60b1d7e3a550a7dd33c44230a979ac2a68
Jahlma <jahlma@kth.se>
2026-05-14 12:31:25 +0200

    Delete DBdata/data_department.sql

--- DBdata/data_department.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO department (department_name,manager)
-VALUES
-  ('STOCKHOLM','MR GREEN'),
-  ('GÖTEBORG','MR SVENSSON'),
-  ('MALMÖ','KAJSA'),
-  ('UPPSALA','HERMANN'),
-  ('SÖDERTÄLJE','LOVA');
